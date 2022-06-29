7d2c73d97b75f8a95368f920118d2541deee8499
Paolo V <paolo.viola@cgi.com>
2022-06-29 09:54:32 +0200

    Enhanced ModuleControlPage (SpinningLights)

--- AppClient/Pages/ModuleControlPage.xaml.cs
+++ AppClient/Pages/ModuleControlPage.xaml.cs
@@ -44,9 +44,12 @@ public partial class ModuleControlPage : ContentPage
     #region Segmented Lights
     private void SegmentedCommandPicker_SelectedIndexChanged(object sender, EventArgs e)
     {
+        object selectedItem = SegmentedCommandPicker.SelectedItem;
+        if (selectedItem == null) return;
+
         bool segmentFieldEnabled;
         bool colorFieldEnabled;
-        switch ((string)SegmentedCommandPicker.SelectedItem)
+        switch ((string)selectedItem)
         {
             case "Segment einschalten":
                 segmentFieldEnabled = true;
@@ -83,49 +86,129 @@ public partial class ModuleControlPage : ContentPage
 
     private async void SegmentedSendButton_Clicked(object sender, EventArgs e)
     {
-        switch ((string)SegmentedCommandPicker.SelectedItem)
+        object selectedItem = SegmentedCommandPicker.SelectedItem;
+        if (selectedItem == null) return;
+
+        string response;
+        switch ((string)selectedItem)
         {
             case "Segment einschalten":
-                await SendCommand(device, $"/segment/{SegmentedSegmentIdField.Text}/color/{SegmentedColorIdField.Text}");
+                response = await SendCommand(device, $"/segment/{SegmentedSegmentIdField.Text}/color/{SegmentedColorIdField.Text}");
+                SegmentedSegmentIdField.Text = "";
+                SegmentedColorIdField.Text = "";
                 break;
             case "Segment exklusiv einschalten":
-                await SendCommand(device, $"/xsegment/{SegmentedSegmentIdField.Text}/color/{SegmentedColorIdField.Text}");
+                response = await SendCommand(device, $"/xsegment/{SegmentedSegmentIdField.Text}/color/{SegmentedColorIdField.Text}");
+                SegmentedSegmentIdField.Text = "";
+                SegmentedColorIdField.Text = "";
                 break;
             case "Auf Hintergrundfarbe setzen":
-                await SendCommand(device, "/clear");
+                response = await SendCommand(device, "/clear");
                 break;
             case "Hintergrundfarbe ändern":
-                await SendCommand(device, $"/setBackground/{SegmentedColorIdField.Text}");
+                response = await SendCommand(device, $"/setBackground/{SegmentedColorIdField.Text}"); SegmentedSegmentIdField.Text = "";
+                SegmentedColorIdField.Text = "";
                 break;
             case "Alle Segmente einschalten":
-                await SendCommand(device, $"/on/{SegmentedColorIdField.Text}");
+                response = await SendCommand(device, $"/on/{SegmentedColorIdField.Text}"); SegmentedSegmentIdField.Text = "";
+                SegmentedColorIdField.Text = "";
                 break;
             case "Alle Segmente ausschalten":
-                await SendCommand(device, "/off");
+                response = await SendCommand(device, "/off");
                 break;
-
             default:
                 throw new NotImplementedException();
+        }
+
+        if(response != null)
+            await DisplayAlert("Error: Server responded", response, "OK");
+    }
+    #endregion
+
+    #region Spinning Lights
+    private async void SpinningHighlightButton_Clicked(object sender, EventArgs e)
+    {
+        string response = await SendCommand(device, $"/highlight/{SpinningWidthField.Text}");
+        if(response != null) await DisplayAlert("Error: Server responded", response, "OK");
+    }
+
+    private async void SpinningHighlightSectionButton_Clicked(object sender, EventArgs e)
+    {
+        string response = await SendCommand(device, $"/section/{SpinningStartField.Text}/{SpinningEndField.Text}");
+        if (response != null) await DisplayAlert("Error: Server responded", response, "OK");
+    }
+
+    private async void SpinningEnableButton_Clicked(object sender, EventArgs e)
+    {
+        string response = await SendCommand(device, $"/on/{SpinningColorField.Text}");
+        if (response != null) await DisplayAlert("Error: Server responded", response, "OK");
+    }
+
+    private async void SpinningDisableButton_Clicked(object sender, EventArgs e)
+    {
+        string response = await SendCommand(device, "/off");
+        if (response != null) await DisplayAlert("Error: Server responded", response, "OK");
+    }
+
+    private async void SpinningUpdateButton_Clicked(object sender, EventArgs e)
+    {
+        string response;
+        if (!string.IsNullOrWhiteSpace(SpinningSpeedField.Text))
+        {
+            response = await SendCommand(device, $"/speed/{SpinningSpeedField.Text}");
+            if (response != null) await DisplayAlert("Error: Server responded", response, "OK");
+            SpinningSpeedField.Text = "";
+        }
+
+        if (!string.IsNullOrWhiteSpace(SpinningDirectionField.Text))
+        {
+            response = await SendCommand(device, $"/setDirection/{SpinningDirectionField.Text}");
+            if (response != null) await DisplayAlert("Error: Server responded", response, "OK");
+            SpinningDirectionField.Text = "";
+        }
+
+        if (!string.IsNullOrWhiteSpace(SpinningBackgroundField.Text))
+        {
+            response = await SendCommand(device, $"/setBackground/{SpinningBackgroundField.Text}");
+            if (response != null) await DisplayAlert("Error: Server responded", response, "OK");
+            SpinningBackgroundField.Text = "";
+        }
+
+        if (!string.IsNullOrWhiteSpace(SpinningForegroundField.Text))
+        {
+            response = await SendCommand(device, $"/setForeground/{SpinningForegroundField.Text}");
+            if (response != null) await DisplayAlert("Error: Server responded", response, "OK");
+            SpinningForegroundField.Text = "";
+        }
+
+        if (!string.IsNullOrWhiteSpace(SpinningPixelPerSegmentField.Text))
+        {
+            response = await SendCommand(device, $"/setPps/{SpinningPixelPerSegmentField.Text}");
+            if (response != null) await DisplayAlert("Error: Server responded", response, "OK");
+            SpinningPixelPerSegmentField.Text = "";
+        }
+
+        if (!string.IsNullOrWhiteSpace(SpinningSegmentCountField.Text))
+        {
+            response = await SendCommand(device, $"/setNos/{SpinningSegmentCountField.Text}");
+            if (response != null) await DisplayAlert("Error: Server responded", response, "OK");
+            SpinningSegmentCountField.Text = "";
         }
     }
     #endregion
 
-    private async Task SendCommand(ModuleInfo device, string command)
+    private static async Task<string> SendCommand(ModuleInfo device, string command)
     {
         using HttpClient client = new HttpClient();
 
         try
         {
             using HttpResponseMessage message = await client.GetAsync(device.Host + command).ConfigureAwait(false);
-            if (!message.IsSuccessStatusCode)
-            {
-                string response = await message.Content.ReadAsStringAsync().ConfigureAwait(false);
-                await DisplayAlert("Error: Server responded:", response, "OK");
-            }
+            return message.IsSuccessStatusCode ? null : await message.Content.ReadAsStringAsync().ConfigureAwait(false);
         }
         catch(Exception ex)
         {
-            await DisplayAlert("Error: ", ex.Message, "OK");
+            return ex.Message;
         }
     }
 
