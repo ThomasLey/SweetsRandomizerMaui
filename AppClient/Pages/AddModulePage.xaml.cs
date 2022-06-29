e1e0a09a46e994d895c3183cd48974e3462e26da
BiotTT <Biotfh@gmail.com>
2022-06-29 09:51:21 +0200

    AddModulePageV5

--- AppClient/Pages/AddModulePage.xaml.cs
+++ AppClient/Pages/AddModulePage.xaml.cs
@@ -1,34 +1,47 @@
+using AppClient.DataStore;
 using Java.Lang;
 
 namespace AppClient.Pages;
 
 public partial class AddModulePage : ContentPage
 {
-	public AddModulePage()
-	{
-		InitializeComponent();
-	}
+    public AddModulePage()
+    {
+        InitializeComponent();
+    }
 
-	private async void ButtonAbort_Clicked(object sender, EventArgs e)
-	{
+    private async void ButtonAbort_Clicked(object sender, EventArgs e)
+    {
         await Navigation.PopAsync();
     }
 
-	private async void ButtonSaved_Clicked(object sender, EventArgs e)
-	{
-		string devicename = Devicename.Text;
-		if (string.IsNullOrWhiteSpace(devicename))
-		{ await DisplayAlert("Speichern nicht möglich", "Bitte Gerätenamen eingeben", "OK");  }
+    private async void ButtonSaved_Clicked(object sender, EventArgs e)
+    {
+        string devicename = Devicename.Text;
+        if (string.IsNullOrWhiteSpace(devicename))
+        { await DisplayAlert("Speichern nicht möglich", "Bitte Gerätenamen eingeben", "OK"); return; }
         string url = URL.Text;
         if (string.IsNullOrWhiteSpace(url))
-        { await DisplayAlert("Speichern nicht möglich", "Bitte URL eingeben", "OK"); }
+        { await DisplayAlert("Speichern nicht möglich", "Bitte URL eingeben", "OK"); return; }
         string picker = (string)Picker.SelectedItem;
         if (string.IsNullOrWhiteSpace(picker))
-        { await DisplayAlert("Speichern nicht möglich", "Bitte Typ auswählen", "OK"); }
+        { await DisplayAlert("Speichern nicht möglich", "Bitte Typ auswählen", "OK"); return; }
         string describtion = Describtion.Text;
         if (string.IsNullOrWhiteSpace(describtion))
-        { await DisplayAlert("Speichern nicht möglich", "Bitte Beschreibung eingeben", "OK"); }
-
-
+        { await DisplayAlert("Speichern nicht möglich", "Bitte Beschreibung eingeben", "OK"); return; }
+        ModuleInfo device = new ModuleInfo();
+        switch (
+            picker)
+        {
+            case "SegmentedLights": device.Type = ModuleType.SegmentedLights; break;
+          case "SpinningLights": device.Type = ModuleType.SegmentedLights;  break;
+           case "Webpage": device.Type = ModuleType.Webpage;  break;
+            case "Unkown": device.Type = ModuleType.Unknown; break;
+        }
+        device.Name = devicename;
+        device.Description = describtion;
+        device.Host = url;
+        await Navigation.PopAsync();
+        ModuleStore.RegisterDevice(device);
     }
 }
