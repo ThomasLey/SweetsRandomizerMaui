8a845822b41f056d857dca406f9a1385bf38c18f
BiotTT <Biotfh@gmail.com>
2022-06-29 09:23:03 +0200

    AddModulPageV4 speichern eingebaut prüfen ob eingabe vorhanen abbrechen

--- AppClient/Pages/AddModulePage.xaml.cs
+++ AppClient/Pages/AddModulePage.xaml.cs
@@ -1,3 +1,5 @@
+using Java.Lang;
+
 namespace AppClient.Pages;
 
 public partial class AddModulePage : ContentPage
@@ -10,5 +12,23 @@ public partial class AddModulePage : ContentPage
 	private async void ButtonAbort_Clicked(object sender, EventArgs e)
 	{
         await Navigation.PopAsync();
+    }
+
+	private async void ButtonSaved_Clicked(object sender, EventArgs e)
+	{
+		string devicename = Devicename.Text;
+		if (string.IsNullOrWhiteSpace(devicename))
+		{ await DisplayAlert("Speichern nicht möglich", "Bitte Gerätenamen eingeben", "OK");  }
+        string url = URL.Text;
+        if (string.IsNullOrWhiteSpace(url))
+        { await DisplayAlert("Speichern nicht möglich", "Bitte URL eingeben", "OK"); }
+        string picker = (string)Picker.SelectedItem;
+        if (string.IsNullOrWhiteSpace(picker))
+        { await DisplayAlert("Speichern nicht möglich", "Bitte Typ auswählen", "OK"); }
+        string describtion = Describtion.Text;
+        if (string.IsNullOrWhiteSpace(describtion))
+        { await DisplayAlert("Speichern nicht möglich", "Bitte Beschreibung eingeben", "OK"); }
+
+
     }
 }
